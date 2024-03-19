826a10f06f45a86191862a46e80e0f591ff37dc1
HuyPQ <huyphan1505dut@gmail.com>
2024-03-19 16:56:51 +0700

    [cm6] create User configurable marcos

--- Driver/drivers/Inc/stm32f407xx_gpio.h
+++ Driver/drivers/Inc/stm32f407xx_gpio.h
@@ -37,6 +37,68 @@ typedef struct
 }GPIO_Handle_t;
 
 /***********************************************************
+ *                 DEFINE AND MARCO
+ ***********************************************************/
+
+/*
+ * @GPIO_PIN_NUMBER
+ */
+#define GPIO_PIN_NO_0        0
+#define GPIO_PIN_NO_1        1
+#define GPIO_PIN_NO_2        2
+#define GPIO_PIN_NO_3        3
+#define GPIO_PIN_NO_4        4
+#define GPIO_PIN_NO_5        5
+#define GPIO_PIN_NO_6        6
+#define GPIO_PIN_NO_7        7
+#define GPIO_PIN_NO_8        8
+#define GPIO_PIN_NO_9        9
+#define GPIO_PIN_NO_10       10
+#define GPIO_PIN_NO_11       11
+#define GPIO_PIN_NO_12       12
+#define GPIO_PIN_NO_13       13
+#define GPIO_PIN_NO_14       14
+#define GPIO_PIN_NO_15       15
+
+/*
+ * @GPIO_PIN_MODES
+ * configure the I/O direction mode
+ */
+#define GPIO_MODE_IN         0
+#define GPIO_MODE_OUT        1
+#define GPIO_MODE_ALTFN      2
+#define GPIO_MODE_ANALOG     3
+#define GPIO_MODE_IT_FT      4            // interupt falling edge
+#define GPIO_MODE_IT_RT      5            // interupt rising edge
+#define GPIO_MODE_IT_RFT     6            // interupt falling edge, rising edge trigger
+
+
+/*
+ * @GPIO_OUTPUT_TYPE
+ * configure the output type of the I/O port
+ */
+#define GPIO_OTYPER_PP         0          // Output push-pull (reset state)
+#define GPIO_OTYPER_OD         1          // Output open-drain
+
+/*
+ * @GPIO_OUTPUT_SPEED
+ *  configure the I/O output speed.
+ */
+#define GPIO_OUT_SP_LOW          0          // Low speed
+#define GPIO_OUT_SP_MED          1          // Medium speed
+#define GPIO_OUT_SP_HIGH         2          // High speed
+#define GPIO_OUT_SP_VRHIGH       3          // Very high speed
+
+/*
+ * @GPIO_PUPDR_CONFIG
+ *  configure the I/O pull-up or pull-down
+ */
+#define GPIO_CONFIG_NO_PUPD         0          // No pull-up, pull-down
+#define GPIO_CONFIG_PU             1          // Pull-up
+#define GPIO_CONFIG_PD             2          // Pull-down
+#define GPIO_CONFIG_REV            3          // Reserved
+
+/***********************************************************
 *                      GPIO API
  ***********************************************************/
 
