8528e4a3a0340d195d61cdde45cd09b6b8eee1c5
HuyPQ <huyphan1505dut@gmail.com>
2024-03-20 11:00:01 +0700

    [cm10] implement for read-write api of GPIO driver

--- Driver/drivers/Src/stm32f407xx_gpio.c
+++ Driver/drivers/Src/stm32f407xx_gpio.c
@@ -244,11 +244,128 @@ uint8_t GPIO_DeInit(GPIO_RegMap_t *pGPIOx)
 /******************************************************************************************
  *								Data read and write
  ******************************************************************************************/
-uint8_t GPIO_ReadFromInputPin(GPIO_RegMap_t *pGPIOx, uint8_t pinNumber);
-uint16_t GPIO_ReadFromInputPort(GPIO_RegMap_t *pGPIOx);
-uint8_t GPIO_WriteToOutputPin(GPIO_RegMap_t *pGPIOx, uint8_t pinNumber, uint8_t value);
-uint8_t GPIO_WriteToOutputPort(GPIO_RegMap_t *pGPIOx, uint16_t value);
-uint8_t GPIO_ToggleOutputPin(GPIO_RegMap_t *pGPIOx, uint8_t pinNumber);
+/**
+ * @brief        Reads the input data register (IDR) of a GPIO Pin
+ *
+ * @details      This function reads the current state of a input pin of a GPIO port
+ *               by accessing the input data register (IDR) of the specified GPIO port
+ *               and extracting the state of the pin indicated by the given pin number.
+ *
+ * @param[in]    pGPIOx : hold the base address of GPIOx
+ *
+ * @return       value
+ * @retval       1 or 0
+ *
+ * @Note         shift the value to the LSB. Then performs a bitwise AND operation
+ * 				 with 1 to extract only the least significant bit
+ *
+ */
+uint8_t GPIO_ReadFromInputPin(GPIO_RegMap_t *pGPIOx, uint8_t pinNumber)
+{
+	uint8_t value = 0U;
+	value = (uint8_t)(( pGPIOx->IDR >> pinNumber ) & (0x01));
+	return value;
+}
+
+/**
+ * @brief        Reads the input data register (IDR) of a GPIO port
+ *
+ * @details      This function reads the current state of all pins of a GPIO port
+ *               by accessing the input data register (IDR) of the specified GPIO port.
+ *
+ * @param[in]    pGPIOx : hold the base address of GPIOx
+ *
+ * @return       value : 16-bit unsigned integer representing the state of all pins of the GPIO port.
+ * @retval       1 or 0
+ *
+ * @Note
+ *
+ */
+uint16_t GPIO_ReadFromInputPort(GPIO_RegMap_t *pGPIOx)
+{
+	uint16_t value = 0U;
+	value = (uint16_t)(pGPIOx->IDR);
+	return value;
+}
+
+/**
+ * @brief        Write to output data register (ODR) of a GPIO pin
+ *
+ * @details      This function write the state to a pins of a GPIO port
+ *               by accessing the output data register (IDR) of the specified pin in a GPIO port.
+ *
+ * @param[in]    pGPIOx    : hold the base address of GPIOx
+ * @param[in]    pinNumber : pin number
+ * @param[in]    value     : data to write : 1 or 0
+ *
+ * @return       GPIO_JobResultType
+ * @retval       GPIO_JOB_OK : The job has been finished successfully
+ * @retval       OTHER :  The job fail
+ *
+ * @Note
+ *
+ */
+uint8_t GPIO_WriteToOutputPin(GPIO_RegMap_t *pGPIOx, uint8_t pinNumber, uint8_t value)
+{
+	GPIO_JobResultType eLldRetVal = GPIO_JOB_OK;
+	if (value == GPIO_PIN_SET)
+	{
+		// write 1
+		pGPIOx->ODR |= (1 << pinNumber );
+	}
+	else
+	{
+		// write 0
+		pGPIOx->ODR &= ~(1 << pinNumber );
+	}
+	return eLldRetVal;
+}
+
+/**
+ * @brief        Write to output data register (ODR) of a GPIO port
+ *
+ * @details      This function write the state to all pins of a GPIO port
+ *               by accessing the output data register (IDR) of the specified GPIO port.
+ *
+ * @param[in]    pGPIOx    : hold the base address of GPIOx
+ * @param[in]    value     : data to write to GPIO port
+ *
+ * @return       GPIO_JobResultType
+ * @retval       GPIO_JOB_OK : The job has been finished successfully
+ * @retval       OTHER :  The job fail
+ *
+ * @Note
+ *
+ */
+uint8_t GPIO_WriteToOutputPort(GPIO_RegMap_t *pGPIOx, uint16_t value)
+{
+	GPIO_JobResultType eLldRetVal = GPIO_JOB_OK;
+	pGPIOx->ODR = value;
+	return eLldRetVal;
+}
+
+/**
+ * @brief        Toggle output of a specific pin in a GPIO port
+ *
+ * @details      This function toggle the state of a GPIO pin in a GPIO port
+ *               by using XOR operator.
+ *
+ * @param[in]    pGPIOx    : hold the base address of GPIOx
+ * @param[in]    pinNumber : pin number
+ *
+ * @return       GPIO_JobResultType
+ * @retval       GPIO_JOB_OK : The job has been finished successfully
+ * @retval       OTHER :  The job fail
+ *
+ * @Note
+ *
+ */
+uint8_t GPIO_ToggleOutputPin(GPIO_RegMap_t *pGPIOx, uint8_t pinNumber)
+{
+	GPIO_JobResultType eLldRetVal = GPIO_JOB_OK;
+	pGPIOx->ODR ^= (1 << pinNumber );
+	return eLldRetVal;
+}
 
 /******************************************************************************************
  *								IRQ Configuration and ISR handling
