702ba02e61e6276fb04f53a645de94055247ee5a
HuyPQ <huyphan1505dut@gmail.com>
2024-04-04 22:59:12 +0700

    [cm17] spi configuration marco for user to init spi module

--- Driver/drivers/Inc/stm32f407xx_spi.h
+++ Driver/drivers/Inc/stm32f407xx_spi.h
@@ -32,6 +32,55 @@
 *                                          CONSTANTS
 ==================================================================================================*/
 
+/*
+ * @SPI_DeviceMode
+ */
+#define SPI_DEVICE_MODE_MASTER         1
+#define SPI_DEVICE_MODE_SLAVE          0
+
+/*
+ * @SPI_BusConfig
+ */
+#define SPI_BUS_CONFIG_FD                  1       // full duplex
+#define SPI_BUS_CONFIG_FD                  2       // half duplex
+#define SPI_BUS_CONFIG_SIMPLEX_RXONLY      3
+
+/*
+ * @SPI_SclkSpeed
+ */
+#define SPI_SCLK_SPEED_DIV2                  0     // fPCLK/2
+#define SPI_SCLK_SPEED_DIV4                  1     // fPCLK/4
+#define SPI_SCLK_SPEED_DIV8                  2     // fPCLK/8
+#define SPI_SCLK_SPEED_DIV16                 3     // fPCLK/16
+#define SPI_SCLK_SPEED_DIV32                 4     // fPCLK/32
+#define SPI_SCLK_SPEED_DIV64                 5     // fPCLK/64
+#define SPI_SCLK_SPEED_DIV128                6     // fPCLK/128
+#define SPI_SCLK_SPEED_DIV256                7     // fPCLK/256
+
+/*
+ * @SPI_DFF
+ */
+#define SPI_DFF_8_BIT                        0     // 8-bit data frame format is selected for transmission/reception
+#define SPI_DFF_16_BIT                       1     // 16-bit data frame format is selected for transmission/reception
+
+/*
+ * @SPI_CPOL
+ */
+#define SPI_CPOL_LOW         0             // CK to 0 when idle
+#define SPI_CPOL_HIGH        1             // CK to 1 when idle
+
+/*
+ * @SPI_CPHA
+ */
+#define SPI_CPHA_LOW          0           // The first clock transition is the first data capture edge
+#define SPI_CPHA_HIGH         1           // The second clock transition is the first data capture edge
+
+/*
+ * @SPI_SSM
+ */
+#define SPI_SSM_DI            0            // Software slave management disabled
+#define SPI_SSM_EN            1            // Software slave management enabled
+
 /*==================================================================================================
 *                                      DEFINES AND MACROS
 ==================================================================================================*/
@@ -63,7 +112,7 @@ typedef struct
 	uint8_t SPI_SclkSpeed;       // Clock speed
 	uint8_t SPI_DFF;             // data frame format (8/16 bit)
 	uint8_t SPI_CPOL;            // Cpol configuration.
-    uint8_t SPI_CPAL;            // Cpal configuration.
+    uint8_t SPI_CPHA;            // Cpha configuration.
 	uint8_t SPI_SSM;             // slave management (software/hardware)
 }SPI_Config_t;
 
